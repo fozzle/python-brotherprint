484d32abe814aef54b30f1d9399ac54798648911
Kyle Petrovich <kylepetrovich@gmail.com>
2015-09-24 14:18:32 -0400

    setup.py version bump
    to take in bugfix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='brotherprint',
-    version='0.1.1',
+    version='0.1.2',
     author='Kyle Petrovich',
     author_email='kylepetrovich@gmail.com',
     packages=['brotherprint', 'brotherprint.test'],
