f20436a5e8106a7f4fef2f831b4dee8bdb94aa84
Kyle Petrovich <kylepetrovich@gmail.com>
2016-01-12 14:01:02 -0500

    README file

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='brotherprint',
-    version='0.1.2',
+    version='0.1.3',
     author='Kyle Petrovich',
     author_email='kylepetrovich@gmail.com',
     packages=['brotherprint', 'brotherprint.test'],
     url='http://github.com/fozzle/python-brotherprint',
     license='LICENSE.txt',
     description='Wrapper for Brother networked label printing commands.',
-    long_description=open('README.md').read(),
+    long_description=open('README').read(),
 )
